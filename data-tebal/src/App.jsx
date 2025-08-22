d5058fbc043593d979f56041a90ab593eb5949c6
Brijeshpatel1712 <brijeshsangani088@gmail.com>
2025-08-22 11:02:23 +0530

    Update App.jsx

--- data-tebal/src/App.jsx
+++ data-tebal/src/App.jsx
@@ -1,11 +1,9 @@
-
-import { Link, Links } from 'react-router-dom'
-import Allrautse from './Routes/Allrautse'
+import { Link } from 'react-router-dom';
+import Allrautse from './Routes/Allrautse';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
-
 
 function App() {
 
@@ -15,29 +13,31 @@ function App() {
   };
 
   return (
-   
-< > 
-<Navbar>
-        <Container className='mainpage' >
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="brijesh ">
-          <div className="links"><Link to={"/Home"} id="brijesh" >home</Link>
-             <Link to={"/Product"} id="brijesh" >product</Link>
-                <Link to={"/Addproduct"} id="brijesh" >Addproduct</Link></div>
-                  <div className="nisarg">
-                      <Link to={"/login"} id="brijesh" >Login </Link>
-                    <button onClick={handleLogout}>logout</button>  
-                  </div>
+    <>
+      <Navbar expand="lg" className="custom-navbar">
+        <Container className='mainpage'>
+          <Navbar.Brand href="#">Navbar</Navbar.Brand>
+
+          <Nav className="nav-sections">
+            {/* Left side links */}
+            <div className="nav-left">
+              <Link to="/Home" className="nav-link">Home</Link>
+              <Link to="/Product" className="nav-link">Product</Link>
+              <Link to="/Addproduct" className="nav-link">Add Product</Link>
+            </div>
+
+            {/* Right side login/logout */}
+            <div className="nav-right">
+              <Link to="/login" className="nav-link">Login</Link>
+              <button className="logout-btn" onClick={handleLogout}>Logout</button>
+            </div>
           </Nav>
         </Container>
       </Navbar>
 
-
-<Allrautse/>
-
-    </  >
-
-  )
+      <Allrautse />
+    </>
+  );
 }
 
-export default App
+export default App;
